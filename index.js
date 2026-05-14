7bb48eb84d1c7a51448e0dbc167ca05968f3295f
Christin115 <cmjordan115@gmail.com>
2026-05-14 19:27:51 -0400

    tests passed

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ function createLoginTracker(userInfo) {
 
     let attemptCount = 0;
 
-    // Inner arrow function
     const login = (passwordAttempt) => {
 
-        // Lock account AFTER 3 failed attempts
         if (attemptCount >= 3) {
             return "Account locked due to too many failed login attempts";
         }
 
-        // Increment attempts
         attemptCount++;
 
-        // Successful login
         if (passwordAttempt === userInfo.password) {
             return "Login successful";
         }
 
-        // Failed login
         return `Attempt ${attemptCount}: Login failed`;
     };
 
